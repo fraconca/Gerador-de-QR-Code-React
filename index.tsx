367d3cbc9191652a70c68dcafa9a3063edd5a26d
fraconca <fraconca@hotmail.com>
2022-08-03 05:57:35 -0300

    QR Code React

--- index.tsx
+++ index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import QRCode from 'react-qr-code';
 
 ReactDOM.render(
-  <QRCode value="https://www2.naveia.com.br/" />,
+  <QRCode value="https://www.google.com.br/" />,
   document.getElementById('root')
 );
